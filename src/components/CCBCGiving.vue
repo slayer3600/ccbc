add334ccf5888d6c91000c9a4ef9d587b129a50d
Jeremy Huntsman <slayer3600@hotmail.com>
2021-03-28 22:17:13 -0700

    Icons and images

--- src/components/CCBCGiving.vue
+++ src/components/CCBCGiving.vue
@@ -3,22 +3,32 @@
     <ActionBar class="actionBar" title="Giving">
       <NavigationButton text="Back" />
     </ActionBar>
-    <WebView
-      src="https://christcommunitypuyallup.churchcenter.com/giving"
-      @loadStarted="onLoadedStarted"
-      @loadFinished="onLoaded"
-    />
+
+    <GridLayout>
+      <WebView
+        src="https://christcommunitypuyallup.churchcenter.com/giving"
+        @loadStarted="onLoadedStarted"
+        @loadFinished="onLoaded"
+      />
+      <ActivityIndicator :busy="isBusy" />
+    </GridLayout>
   </Page>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isBusy: true,
+    };
+  },
   methods: {
     onLoadedStarted() {
       console.log('Loading Started...');
     },
     onLoaded() {
       console.log('Loading completed...');
+      this.isBusy = false;
     },
   },
 };
